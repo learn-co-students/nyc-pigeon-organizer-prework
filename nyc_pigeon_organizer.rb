8dda93b8a16a4a73d5e9e927c9878ada32308f17
Hersh Patel <hershe.91@gmail.com>
2017-03-18 20:06:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,54 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-end
+    pigeon_list = {}
+
+    data.each do |category, sub_category|
+      sub_category.each do |detail, names|
+        names.each do |pigeon|
+        if pigeon_list.include?(pigeon) == false
+
+          pigeon_list[pigeon] = {
+            :color => [],
+            :gender => [],
+            :lives => []
+          }
+
+
+
+        end
+
+
+
+        end
+
+        pigeon_list.each do |new_name, value|
+
+          if names.include?(new_name)
+
+            value[category] << detail.to_s
+
+
+
+
+
+
+
+
+
+
+
+
+
+          end
+
+        end
+
+      end
+
+
+
+    end
+
+  pigeon_list
+
+  end

34ca46b0cd1292740cb76d749bdf0a9dd7099107
Mike Merin <mikemerinweather@gmail.com>
2017-03-19 03:59:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  list = {}
+  data.each do |subject, type|
+    type.each do |t,a|
+      a.each do |x|
+        list[x] = {color: [], gender: [], lives: [] }
+      end
+    end
+  end
+  data.each do |subject, type|
+    type.each do |t,a|
+      a.each do |x|
+        list[x][:color] << t.to_s if subject == :color
+        list[x][:gender] << t.to_s if subject == :gender
+        list[x][:lives] << t if subject == :lives
+      end
+    end
+  end
+  list
 end

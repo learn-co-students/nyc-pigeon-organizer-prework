af7158167f6c200b57633f8d731d1006d794bc06
Jack he <jackhe678@gmail.com>
2017-03-30 21:33:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+
+  data.each do |k1, v1|
+    v1.each do |k2, v2|
+      v2.each do |names|
+        if !new_hash[names]
+          new_hash[names] = {k1 => [k2.to_s]}
+        elsif !new_hash[names][k1]
+          new_hash[names][k1] = [k2.to_s]
+        else
+          new_hash[names][k1] << k2.to_s
+        end
+      end
+    end
+  end
+
+  new_hash
 end

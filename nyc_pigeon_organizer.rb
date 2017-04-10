7c6794a3281192bb428921184810bd3fc50a8ed6
Luis Arita <luis@ccc-it.net>
2017-04-10 20:36:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each do |property, value|
+    value.each do |option, members|
+      members.each do |member|
+        if !new_hash.include?(member)
+          new_hash[member] = {}
+        end
+        if new_hash[member][property] == nil
+          new_hash[member][property] = []
+        end
+        new_hash[member][property].push(option.to_s)
+      end
+    end
+  end
+  new_hash
 end

ccacf4b610775dc52211778cdd4a88748ce015ea
drzly777 <andrew.zheng725@gmail.com>
2017-04-17 12:01:21 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_pigeon_hash = {}
+  data.each {|category, attributes|
+    attributes.each {|attribute,pigeons|
+      pigeons.each {|pigeon|
+        if new_pigeon_hash.keys.member?(pigeon)
+          if new_pigeon_hash[pigeon].keys.member?(category)
+            new_pigeon_hash[pigeon][category] << attribute.to_s
+          else
+            new_pigeon_hash[pigeon][category] = [attribute.to_s]
+          end
+        else
+          new_pigeon_hash[pigeon] = {category => [attribute.to_s]}
+        end
+      }
+    }
+
+  }
+  new_pigeon_hash
 end

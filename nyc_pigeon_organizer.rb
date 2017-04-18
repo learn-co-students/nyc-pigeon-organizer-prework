9e51adecee9099ff879f800f864eca13f57bc8b6
doulighan <doulighan@gmail.com>
2017-04-18 16:52:36 -0400

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require 'pry'
+
+def nyc_pigeon_organizer(pigeon_data)
+  organized_pigeons = {}
+  pigeon_data.each do |attributes, data_hash|
+    data_hash.each do |catagory, names_array|
+      names_array.each do |name|
+        if organized_pigeons[name].nil?
+          organized_pigeons[name] = {
+            color: [],
+            gender: [],
+            lives: []
+          }
+        end
+        organized_pigeons[name][attributes] << catagory.to_s 
+      end
+    end
+  end
+  organized_pigeons
 end

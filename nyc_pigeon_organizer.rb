77e7b9e9ef3ff3a0d86d18aa1e47aeec25323bf7
Alexander Boring <alexboring@hotmail.com>
2017-04-19 17:55:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
+  organized_pigeon = {}
+
+  data.each do | category, attributes |
+    attributes.each do | attribute, pigeons |
+      pigeons.each do |pigeon|
+        organized_pigeon[pigeon] ||= {}
+        organized_pigeon[pigeon][category] ||= []
+        organized_pigeon[pigeon][category] << attribute.to_s
+      end
+    end
+  end
+
+  organized_pigeon
   # write your code here!
 end

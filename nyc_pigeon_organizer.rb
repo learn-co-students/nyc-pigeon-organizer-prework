ef9c995327b6816ce2703c17d384903cca67517b
Mike Luo <zluo@alumni.berklee.edu>
2017-04-20 00:26:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  color = data[:color]
+  gender = data[:gender]
+  lives = data[:lives]
+
+  color.each do |color, array|
+    array.each do |bird|
+      if !pigeon_list.keys.include?(bird)
+        pigeon_list[bird] = {:color => [], :gender => [], :lives => []}
+        pigeon_list[bird][:color] = ["#{color}"]
+      else
+        pigeon_list[bird][:color] << "#{color}"
+      end
+    end
+  end
+
+  gender.each do |gender, array|
+    array.each { |bird| pigeon_list[bird][:gender] << "#{gender}" }
+  end
+
+  lives.each do |location, array|
+    array.each { |bird| pigeon_list[bird][:lives] << "#{location}" }
+  end
+
+  pigeon_list
 end

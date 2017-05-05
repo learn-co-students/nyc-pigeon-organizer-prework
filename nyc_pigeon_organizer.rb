9698b6f7a9b0145cb48f562aff6e89a760a3fe5b
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-05-05 20:01:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized = {}
+  data.each do |attribute, value|
+    data[attribute].each do |key, value|
+      data[attribute][key].each do |x|
+          organized[x] = {}
+      end
+    end
+  end
+
+  organized.each do |key, value|
+    data.each do |k, value|
+      organized[key][k] = []
+    end
+  end
+
+  data.each do |attribute, value|
+    data[attribute].each do |key, value|
+      organized.each do |k, v|
+        if data[attribute][key].include?(k)
+          organized[k][attribute].push(key.to_s)
+        end
+      end
+    end
+  end
+  organized
 end

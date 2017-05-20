4896f01c87d4b4a86b179130b7388a0bcffb23a2
James Rhee <jamesshrhee@gmail.com>
2017-05-20 04:27:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+  names(data).each do |name|
+    new_data[name] = {}
+    data.each do |category, value|
+      new_data[name][category] = fetch(value, name)
+    end
+  end
+  new_data
+end
+
+def names(data)
+  males = data[:gender][:male]
+  females = data[:gender][:female]
+  males + females
+end
+
+def fetch(value, name)
+  value.map do |type, list|
+    type.to_s if list.include?(name)
+  end.compact
 end

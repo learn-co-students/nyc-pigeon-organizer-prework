095ebc33e3435731da4809cb311cc7a3679d7475
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-06-02 18:42:45 -0400

    pigeons = organized

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_name_array = []
+  pigeon_list = {}
+  data.each do |attributes, variants|
+    variants.each do |type, pigeons|
+      pigeon_name_array << pigeons
+    end
+  end
+  pigeon_name_array.flatten!.uniq!
+  pigeon_name_array.each do |pigeon|
+    pigeon_list[pigeon] = {}
+  end
+  pigeon_list.each do |pigeon, attributes|
+    data.each do |att, var|
+      pigeon_list[pigeon][att] = []
+      var.each do |type, pigeons|
+        pigeons.each do |name|
+          if pigeon == name
+            pigeon_list[pigeon][att] << type.to_s
+          end
+        end
+      end
+    end
+  end
+  pigeon_list
 end

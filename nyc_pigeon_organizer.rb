293973aa3ec7e9ffeaf2fb2a4dd15cbf197aa711
joshlacey <williamjoshualacey@gmail.com>
2017-07-10 18:07:05 -0400

    done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    pigeon_list = {}
+    data.each do |subdata, list|
+        if subdata == :lives
+            list.each do |key, array|
+                array.each do |element|
+                    pigeon_list[element] = {}
+                end
+            end
+        end
+    end
+    data.each do |subdata, list|
+        list.each do |attribute, array|
+            array.each do |name|
+                pigeon_list[name][subdata] = []
+            end
+        end
+    end
+    pigeon_list.each do |name, attribute|
+        attribute.each do |subattribute, array|
+            data[subattribute].each do |key, value|
+                if data[subattribute][key].include? name
+                    pigeon_list[name][subattribute] << key.to_s
+                    end
+            end
+        end
+    end
+    pigeon_list
 end

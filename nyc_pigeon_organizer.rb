04b6eb7b86097ad36a92e93f381ee383be50f0c6
sophiehelf <sophie.helf@gmail.com>
2017-07-14 20:46:20 -0700

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+	new_hash = {}
+	data.each do |attribute, info|
+		info.each do |info, arr|
+			arr.each do |element|
+				if !new_hash.has_key?(element) # add keys to new hash
+					new_hash[element] = {}
+				end
+			end
+		end
+	end
+	data.each do |attribute, info|
+		info.each do |info, arr|
+			arr.each do |element| 
+				new_hash[element][attribute] = [] # add attributes to each key
+			end 
+		end
+	end
+	data.each do |attribute, info|
+		info.each do |info, arr|
+			arr.each do |element|
+				if new_hash.key?(element)
+					new_hash[element][attribute] << info.to_s
+				end
+			end
+		end
+	end
+	new_hash
 end

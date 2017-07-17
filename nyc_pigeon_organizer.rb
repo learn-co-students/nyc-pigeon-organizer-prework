c0412dd75e8e53d04800faa49bae5037c0c71dae
David Squires <david.squires1@gmail.com>
2017-07-17 20:22:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, attribute_data|
+    attribute_data.each do |attribute_instance, pigeons|
+      pigeons.each do |pigeon|
+        if !pigeon_list.include? pigeon
+          pigeon_list[pigeon] = {}
+        end
+        pigeon_list[pigeon][attribute] = [] if !pigeon_list[pigeon][attribute]
+        pigeon_list[pigeon][attribute] << attribute_instance.to_s
+      end
+    end
+  end
+  pigeon_list
 end

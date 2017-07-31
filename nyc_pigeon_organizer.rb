1de72330744e4af6f9a0e0b013b836220660270a
github username <github email address>
2017-07-31 14:38:34 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  names = data[:gender][:male] + data[:gender][:female]
+  puts names
+  new_pigeons = {}
+  names.each { |name|
+    new_pigeons[name] = {
+      color: data[:color].collect{|k,v| k.to_s if v.include?(name)}.compact,
+      gender: data[:gender].collect{|k,v| k.to_s if v.include?(name)}.compact,
+      lives: data[:lives].collect{|k,v| k.to_s if v.include?(name)}.compact,
+    }
+  }
+  new_pigeons
 end

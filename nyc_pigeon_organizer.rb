5726c2acd8f760a020d79ceac571956521e753bb
xxDOOMbox <shawwian@gmail.com>
2017-08-06 19:32:59 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = Hash.new 
+  data.each do |attribute, value|
+  	value.each do |attr, val|
+	  val.each do |v|
+  	    pigeons[v] = {
+  	    color: [],
+  	    gender: [],
+  	    lives: []
+	    } 
+	  end
+  	end 
+  end 
+  
+  pigeons.each do |name, stats| 
+  	stats.each do |stat, values|
+	  data.each do |attribute, value|
+	    value.each do |attr, val| 
+	      if val.include?(name) && !stats[attribute].include?(attr.to_s)
+	      	stats[attribute] << attr.to_s
+	      end
+	    end 
+	  end 
+	end 
+  end
+
 end

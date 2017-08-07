7b72539787050f8be4ae3a29b7c6acef47135c85
Luigi Pepe <luigipepe90@gmail.com>
2017-08-07 13:26:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  newData = {}
+  data[:gender].each do |key, value|
+    value.each do |elem|
+      newData[elem] = {}
+      newData[elem][:color] = []
+      newData[elem][:gender] = []
+      newData[elem][:lives] = []
+    end
+  end
+
+  data.each do |key, value|
+    puts "key #{key}"
+    value.each do |key2, value2|
+      puts "key2 #{key2}"
+      value2.each do |elem|
+        puts "elem #{elem}"
+        newData[elem][key].push(key2.to_s)
+      end
+    end
+  end
+  return newData
 end

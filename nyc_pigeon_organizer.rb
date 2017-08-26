3a9084f779d6391d041b321cc64d328445f49e16
mattgellert <mattgellert@gmail.com>
2017-08-26 14:11:51 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,37 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  name_arr = []
+  color = {}
+  gender = {}
+  lives = {}
+
+  data.each {|item, details|
+    details.each {|detail, pigeons|
+      pigeons.each {|name|
+        if item == :color
+          if name_arr.include?(name) == false
+            name_arr << name
+            color[name] = [detail.to_s]
+          elsif name_arr.include?(name)
+            color[name] << detail.to_s
+          end
+        end
+
+        if item == :gender
+            gender[name] = [detail.to_s]
+        end
+
+        if item == :lives
+            lives[name] = [detail.to_s]
+        end
+      }
+    }
+  }
+
+
+  pigeon_list = {}
+  name_arr.each {|pigeon|
+    pigeon_list[pigeon] = {:color => color[pigeon], :gender => gender[pigeon],:lives => lives[pigeon]}
+  }
+  pigeon_list
 end

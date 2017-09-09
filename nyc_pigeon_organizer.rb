912ef9996b1fa4f1618211a4f98eddb3118af58e
jordan <jcstorms25@gmail.com>
2017-09-09 18:05:51 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |key, val|
+    val.each do |attribute, names|
+      names.each do |name|
+        new_hash[name] ||= {}
+        new_hash[name][key] ||= []
+        new_hash[name][key] << attribute.to_s
+      end
+    end
+  end
+  new_hash
 end

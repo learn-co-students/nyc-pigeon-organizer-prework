0919e715a42f031a9cce8f964f35f2d5c8ea0d82
Anthony Giuliano <agiuliano@gmail.com>
2017-09-17 14:44:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.each do |main_attr, hsh|
+    hsh.each do |sub_attr, ary|
+      ary.each do |name|
+        result[name] ||= {}
+        result[name][main_attr] ||= []
+        result[name][main_attr] << sub_attr.to_s
+      end
+    end
+  end
+  result
 end

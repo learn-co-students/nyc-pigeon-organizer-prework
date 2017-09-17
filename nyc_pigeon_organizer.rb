d5acd209b34aca00263f3e852f9b8d426156e4b7
Shirley Lin <s.lin12@gmail.com>
2017-09-17 22:13:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |cat, attr_hash|
+    attr_hash.each do |attribute, name_array|
+      name_array.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {color: [], gender: [], lives: []}
+          pigeon_list[name][cat] << attribute.to_s
+        else
+          pigeon_list[name][cat] << attribute.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end

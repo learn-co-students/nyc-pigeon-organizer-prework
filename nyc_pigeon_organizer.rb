a63060733c88fd0556282fad32c211548fc6613e
mmb16 <mmb583@nyu.edu>
2017-10-18 15:52:37 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |category, data|
+    data.each do |attribute, array|
+      array.each do |name|
+        #binding.pry
+        pigeon_list[name] ||= {}
+        pigeon_list[name][category] ||= []
+        pigeon_list[name][category] << attribute.to_s
+        #binding.pry
+      end
+    end
+  end
+  pigeon_list
 end

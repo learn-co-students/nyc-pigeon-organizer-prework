ea722cbcd44aa7910a393881dcaad36f18c97fb8
David Tenenbaum <david.tenenbaum87@gmail.com>
2017-11-17 18:52:16 +0000

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+
+  data.each do |attributes, sub_attributes|
+    sub_attributes.each do |sub_atts, names|
+      names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][attributes] ||= []
+        pigeon_list[name][attributes] << sub_atts.to_s
+      end
+    end
+  end
+
+  pigeon_list
 end

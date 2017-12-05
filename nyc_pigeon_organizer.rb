f1df0ef2d90f451c2c7ce95a80d83540b12cbfdf
TorreJohnson <torre.johnson@gmail.com>
2017-12-04 23:20:10 -0500

    defined #nyc_pigeon_organizer

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |cgl, cgl_hash|
+    cgl_hash.each do |char, name_arr|
+      name_arr.each do |name|
+        if !pigeon_list.has_key?(name)
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name].has_key?(cgl)
+          pigeon_list[name][cgl] = [char.to_s]
+        else
+          pigeon_list[name][cgl] << char.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end

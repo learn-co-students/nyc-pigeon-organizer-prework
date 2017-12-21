f0ae95883e85b10318541c6e96b3cd80a0f066f8
morgannegagne <morgannegagne@gmail.com>
2017-12-21 11:30:21 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,49 @@
+require "pry"
+data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # set pidgeon names as keys
+
+  organized_pigeons = {}
+
+  pigeon_names = []
+
+  # find all pigeon names
+  data[:gender].each {|gender, gender_names| pigeon_names << gender_names}
+
+  # set pigeon names as the keys of organized_pidgeons hash
+  pigeon_names = pigeon_names.flatten
+  pigeon_names.each {|pigeon_name| organized_pigeons[pigeon_name] = {} }
+
+  # fill in traits for each pigeon
+  data.each do |characteristic, characteristic_data| #color, gender, lives
+    characteristic_data.each do |trait, pigeons_with_trait| #color: purple, grey, brown, white
+      pigeons_with_trait.each do |pigeon_with_trait|
+        if organized_pigeons[pigeon_with_trait][characteristic]
+          organized_pigeons[pigeon_with_trait][characteristic] << trait.to_s
+        else
+          organized_pigeons[pigeon_with_trait][characteristic] = [trait.to_s]
+        end
+      end
+    end
+  end
+  organized_pigeons
 end

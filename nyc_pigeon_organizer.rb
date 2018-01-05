8e02a926d472a2665ca148131a3e23d440e4bdcd
Christopher Diep <diep.christopher@gmail.com>
2018-01-05 08:57:40 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  pigeon_names = []
+  data.each do |attributes,attr_data|
+    attr_data.each do |spec,pigeon_array|
+      pigeon_names = pigeon_names + pigeon_array
+    end
+  end
+  pigeon_names.uniq
+  binding.pry
+  pigeon_list
 end

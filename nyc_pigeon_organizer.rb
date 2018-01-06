2430ce82da71579d807a77593b42585929e03dcc
Christopher Diep <diep.christopher@gmail.com>
2018-01-06 11:35:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,8 @@ require "pry"
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
 
+  #goes deep into the old hash and reformats one by one (iteration of nested)
+
   data.each do |attribute,attr_data|
     attr_data.each do |spec,pigeon_array|
       pigeon_array.each do |name|

f77a868a6c086d76b60e92d1db15bf8cf7ab0622
Zach Vary <zach.vary@gmail.com>
2018-01-08 21:11:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, pigeon_info|
+    pigeon_info.each do |pigeon_info, name|
+      name.each do |i|
+        if !pigeon_list.has_key?(i)
+          pigeon_list[i] = {}
+        end
+        if !pigeon_list[i].has_key?(attribute)
+          pigeon_list[i][attribute] = []
+        end
+        if !pigeon_list[i][attribute].include?(pigeon_info)
+          pigeon_list[i][attribute] << pigeon_info.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end

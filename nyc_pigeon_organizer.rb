1f8f0d5a762fe1be3f0c05392f7a9343f4e47f48
Jason Lim <jayugc@gmail.com>
2018-01-15 17:46:49 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  list = {}
+
+  data.each do |category, trait|
+    trait.each do |trait, names|
+      names.each do |name|
+
+        if list[name].nil?
+          list[name] = {}
+        end
+        if list[name][category].nil?
+          list[name][category] = []
+        end
+        if list[name][category].is_a? Array
+          list[name][category] << trait.to_s
+        end
+      end
+    end
+  end
+  list
 end

4a1ca11910805831f6f107590ca75f2031770a24
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-08 03:52:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  allPigeonNames = []
+  newData = {}
+  data[:gender].each {|gender, pigeons|
+    pigeons.each {|pigeon|
+      allPigeonNames.push(pigeon)
+    }
+  }
+  allPigeonNames.each {|pigeonName|
+    if !(newData[pigeonName])
+      newData[pigeonName] = {}
+    end
+    data.each {|attribute, statuses|
+      statuses.each {|status, pigeons|
+        pigeons.each {|pigeon|
+          if pigeonName == pigeon
+            if newData[pigeonName][attribute]
+              newData[pigeonName][attribute].push(status.to_s)
+            else
+              newData[pigeonName][attribute] = [status.to_s]
+            end
+          end
+        }
+      }
+    }
+  }
+  newData
 end

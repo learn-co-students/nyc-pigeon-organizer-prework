538eac25cb0bcf0677994e17841f0016a86fa147
Ray Blasko <mray6288@gmail.com>
2018-02-08 10:19:27 -0600

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    new_data = {}
+    data.each do |attribute, att_data|
+        att_data.each do |option, pigeons|
+            pigeons.each do |pigeon|
+                if !new_data.has_key?(pigeon)
+                    new_data[pigeon] = {}
+                end
+                if !new_data[pigeon].has_key?(attribute)
+                    new_data[pigeon][attribute] = []
+                end
+                new_data[pigeon][attribute] << option.to_s
+            end
+        end
+    end
+    new_data
 end

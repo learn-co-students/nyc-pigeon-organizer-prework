73a5968e9bd730e08ad8f9ee105bdf2e08e52c11
github username <github email address>
2018-02-09 00:29:05 -0500

    hash madness

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each{|attribute, types_hash|
+  types_hash.each{|type, name_array|
+    typ = type.to_s
+    name_array.each{|name|
+    if new_hash[name] == nil
+      new_hash[name] = {}
+      new_hash[name][attribute] = []
+      new_hash[name][attribute].push(typ)
+    elsif new_hash[name][attribute] == nil
+      new_hash[name][attribute] = []
+      new_hash[name][attribute].push(typ)
+    else
+      new_hash[name][attribute].push(typ)
+    end
+      }
+    }
+  }
+#  binding.pry
+  new_hash
 end

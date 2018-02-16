fa52d23397a94d28709194be6e6dc2ef9eddecc8
victoria-huang <vctrah@gmail.com>
2018-02-16 10:24:41 -0500

    reformatting hash

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    organized = {}
+    
+    data.each do |attribute, types|
+        types.each do |type, names|
+            names.each do |name|
+                if organized[name] == nil
+                    organized[name] = {attribute => [type.to_s]}
+                elsif organized[name][attribute] == nil
+                    organized[name][attribute] = [type.to_s]
+                else
+                    organized[name][attribute] << type.to_s
+                end
+            end
+        end
+    end
+    
+    organized
 end
+

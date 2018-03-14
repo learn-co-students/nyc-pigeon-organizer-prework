116792e823dc2ea0f97712b5adaa7cca5bf6c990
William Dale <williambdale@gmail.com>
2018-03-14 17:47:57 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+require 'pry'
+
+def nyc_pigeon_organizer(pigeon_data)
+  pigeons = {}
+  pigeon_data.each do |attributes, info|
+    pigeon_data.keys.each do |attribute|
+      pigeon_data[attribute].each do |key,value|
+        value.each do |name|
+          if pigeons[name] == nil 
+            pigeons[name] = {}
+          elsif pigeons.keys.include?(name) && pigeons[name][attribute]
+            pigeons[name][attribute] << key.to_s unless pigeons[name][attribute].include?(key.to_s)
+          else 
+            pigeons[name][attribute] = [key.to_s]
+          end 
+        end 
+      end 
+    end 
+  end 
+  pigeons
+end 

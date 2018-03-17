83dddf396938d24049db6cb4c24909b8f92ad89c
Adam Cohn <adamcohn88@gmail.com>
2018-03-16 22:17:44 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,10 @@
-require 'pry'
-
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each { |key, value|
     value.each { | color, name |
       name.each {|pidgeon|
       if new_hash[pidgeon] == nil
-        new_hash[pidgeon] = {key => [color.to_s]}
+        new_hash[pidgeon] = {key => [color.to_s]}   
       elsif new_hash[pidgeon][key] == nil
         new_hash[pidgeon][key] = [color.to_s]
       else

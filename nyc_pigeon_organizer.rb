aac0af466758d163afa806386e4cfb38c001473a
Yong-Nicholas Kim <yongnicholaskim@gmail.com>
2018-03-18 05:10:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  output_hash = {}
+  
+  data.map do | attribute, detail_hash |
+    detail_hash.map do | detail, name_array |
+      name_array.map do | name |
+        this_detail = {name => {attribute => []}}
+        output_hash = output_hash.merge!(this_detail) {|key, output, this| output.merge(this) }
+      end
+    end
+  end
+  
+  data.map do | attribute, detail_hash |
+    detail_hash.map do | detail, name_array |
+      name_array.map do | name |
+        output_hash[name][attribute].push(detail.to_s)
+      end
+    end
+  end
+  
+  output_hash
 end

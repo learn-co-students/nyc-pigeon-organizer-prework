4b75177f7525209b8ad8543b88e05d7175d3b4bf
Mike C <mikecronin92@gmail.com>
2018-03-28 19:08:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |stat,hash|
+    hash.each do |specific,birds| #purple
+      birds.each do |bird|
+        new_hash[bird] ||= {color: [], gender: [], lives: []}
+        new_hash[bird][stat] << specific.to_s 
+      end
+    end
+  end
+  return new_hash
 end

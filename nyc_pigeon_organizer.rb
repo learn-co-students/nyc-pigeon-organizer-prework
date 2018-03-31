8673b681194bf470eb91cb76bc0085cb96334852
Ian Pollack <ianjpollack@gmail.com>
2018-03-31 01:36:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  birdsArray = []
+  newHash = {}
+  data.each do |category, hash|
+    hash.each do |key, array|
+      array.each do |bird|
+        if birdsArray.include?(bird)
+        else
+          birdsArray << bird
+        end
+      end
+    end
+  end
+  i = 0
+  until i == birdsArray.length
+    newHash[birdsArray[i]] = {}
+    newHash[birdsArray[i]][:color] = []
+    newHash[birdsArray[i]][:gender] = []
+    newHash[birdsArray[i]][:lives] = []
+    i += 1
+  end
+  data.each do |category, hash|
+    hash.each do |key, array|
+      array.each do |bird|
+        if data[category][key].include?(bird)
+          newHash[bird][category].push(key.to_s)
+        end
+      end
+    end
+  end
+  return newHash
 end

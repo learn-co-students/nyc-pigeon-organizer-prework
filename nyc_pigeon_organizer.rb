db893a2127edb455d9e304ee54d7d52c5f5378f6
jessebillard <jessebillard@mac.com>
2018-04-05 17:15:26 -0400

    Pigeon Organizer

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    new_hash = {}
+    
+    data.each do |attribute, attribute_hash|
+        attribute_hash.each do |value_key, name_array|
+            name_array.each do |names|
+                new_hash[names] ||= {}
+                new_hash[names][attribute] ||= []
+                new_hash[names][attribute] << value_key.to_s
+            end
+        end
+    end
+    new_hash
 end
+

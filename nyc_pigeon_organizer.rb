a970487fa128a2d6bbf314a07f33a59fbf6a0969
Chris Alberti <christopherjalberti@gmail.com>
2018-04-23 00:50:28 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+
+  data.each do |trait_type, traits|
+    traits.each do |trait, names|
+      names.each do |name|
+        if result.has_key?(name)
+          if result[name].has_key?(trait_type)
+            result[name][trait_type] << trait.to_s
+          else
+            result[name][trait_type] = [trait.to_s]
+          end
+        else
+          result[name] = {trait_type => [trait.to_s]}
+        end
+      end
+    end
+  end
+  result
 end

2edebbcc0ff310a01c365de48c99f7f512a1b7ce
Ian Canino <ian.canino@gmail.com>
2018-04-23 06:25:55 -0400

    implemented the pigeon method

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+
+  data.each do |characteristics, attributes|
+    attributes.each do |value, names|
+      names.each do |name|
+# ||= is useful i you want to do nothing if it does exist but create it if it doesn't exist
+        pigeon_hash[name] ||= {}
+        pigeon_hash[name][characteristics] ||= []
+        pigeon_hash[name][characteristics] << value.to_s
+      end
+    end
+  end
+  pigeon_hash
 end

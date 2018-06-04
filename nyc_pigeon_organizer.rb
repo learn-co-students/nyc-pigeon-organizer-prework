1d8b6e1eca8a82c8aa3dd40c8265e8ffc26d1a92
Stephen F <stephen@farver.io>
2018-06-04 04:25:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  output_hash = {}
+  
+  data.each do |attribute, details|
+    details.each do |detail_name, pigeon_names|
+      pigeon_names.each do |name|
+        if !output_hash[name]
+          output_hash[name] = {}
+        end
+        
+        if !output_hash[name][attribute]
+          output_hash[name][attribute] = []
+        end
+        
+        output_hash[name][attribute] << detail_name.to_s
+      end
+    end
+  end
+  output_hash
 end

1013069283d4f07943e3f7e3425f5707df80c1cc
jaredianmills <jaredianmills@gmail.com>
2018-06-08 18:26:54 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  sorted_pigeons = {}
+  data.each do |color_gender_lives, detail|
+    detail.each do |detail_data, name_array|
+      name_array.each do |name|
+        sorted_pigeons[name] = {}
+        end
+      end
+    end
+  sorted_pigeons.each do |name, attribute|
+    data.each_key do |key|
+      attribute[key] = []
+    end
+  end
+  sorted_pigeons.each do |name, attributes|
+    attributes.each do |category, array|
+      data.each do |color_gender_lives, detail|
+        detail.each do |detail_data, name_array|
+          if name_array.include?(name) && category == color_gender_lives
+            array << detail_data.to_s
+          end
+        end
+      end
+    end
+  end
+  sorted_pigeons
 end

ea7bf24b9e9a648ea4d1e8fcfe984327a19ac6b1
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-06-23 13:37:51 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #:color, :gender, :lives
+  new_hash = {}
+  data.each do |category, hash|
+    hash.each do |key, value|
+      value.each do |name|
+        if new_hash[name] == nil
+          new_hash[name] = {category => [key.to_s]}
+        elsif new_hash[name][category] == nil
+          new_hash[name].merge!({category => [key.to_s]})
+        else
+          new_hash[name][category] << key.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end

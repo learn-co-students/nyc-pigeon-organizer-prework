08033f0528b5bb0b90473f3a6be9d25f24b4019a
Yicheng Jin <bobjin42@gmail.com>
2018-06-25 17:26:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each{|ele, values|
+    values.each{|fac, names|
+      names.each{|name|
+        if new_hash[name] == nil 
+        new_hash[name] = {}
+        new_hash[name][ele] = [fac.to_s]
+        elsif new_hash[name][ele] == nil 
+          new_hash[name][ele] = [fac.to_s]
+        else 
+          new_hash[name][ele] << fac.to_s
+        end
+      }
+    }
+  }
+  new_hash
 end

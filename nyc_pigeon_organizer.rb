775bccfea4330e8b86f4eb87abb39675d5de74e1
github username <github email address>
2018-06-27 18:28:30 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+pigeon_list = {}
+
+data.each do |pigeon_color_gender_lives, cgl_hash|
+  cgl_hash.each do |stats, names|
+    names.each do |name|
+      !pigeon_list.include?(name) ? (pigeon_list[name] = Hash.new) : nil
+        if !pigeon_list[name].include?(pigeon_color_gender_lives)
+          pigeon_list[name][pigeon_color_gender_lives] = Array.new
+          pigeon_list[name][pigeon_color_gender_lives].push(stats.to_s)
+        else
+          pigeon_list[name][pigeon_color_gender_lives].push(stats.to_s)
+      end
+    end
+  end
+end
+  return pigeon_list
 end

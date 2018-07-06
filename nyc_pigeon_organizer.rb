1b67edec1793a8a9dd7b45352b0a730ed8d3dd93
derickcastillo38 <9derick@gmail.com>
2018-07-06 01:46:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  pigeons = []
+
+  data[:color].each do |color, pigeon_array|
+    pigeon_array.each do |pigeon|
+      pigeons << pigeon unless pigeons.include?(pigeon)
+    end
+  end
+
+  pigeons.each do |pigeon|
+    pigeon_list[pigeon] = {:color => [], :gender => [], :lives => []}
+  end
+
+  data[:color].each do |color, color_array|
+    pigeon_list.each do |pigeon, pigeon_array|
+      if data[:color][color].include?(pigeon)
+        pigeon_array[:color] << color.to_s
+      end
+    end
+  end
+
+  data[:gender].each do |gender, gender_array|
+    pigeon_list.each do |pigeon, pigeon_array|
+      if data[:gender][gender].include?(pigeon)
+        pigeon_array[:gender] << gender.to_s
+      end
+    end
+  end
+
+  data[:lives].each do |lives, lives_array|
+    pigeon_list.each do |pigeon, pigeon_array|
+      if data[:lives][lives].include?(pigeon)
+        pigeon_array[:lives] << lives.to_s
+      end
+    end
+  end
+
+  pigeon_list
 end

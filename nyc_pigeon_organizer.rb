fc6014c75e0a193df945e105e8f11dde0ff342cb
github username <github email address>
2018-07-10 17:01:48 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organizer = {}
+  data.each do |attributes, attributes_hash|
+    attributes_hash.each do |style, name|
+      name.each do |pigeon|
+        if organizer[pigeon] == nil
+          organizer[pigeon] = {}
+          organizer[pigeon][attributes] = [style.to_s]
+        elsif organizer[pigeon][attributes] == nil
+            organizer[pigeon][attributes] = [style.to_s]
+        else
+            organizer[pigeon][attributes] << style.to_s
+          end
+      end
+    end
+  end
+  organizer
 end

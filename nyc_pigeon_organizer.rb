7996a7c9bc59f0f1389bc975b1ebe06b461e14d9
github username <github email address>
2018-07-12 12:31:14 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.map do |attribute, categories|
+    # puts "#{attribute}: #{categories.values}"
+
+    #:color, {:purple => {...}}
+
+    categories.map do |details, people|
+      #purple: ["Theo", "Peter Jr.", "Lucky"]
+      #grey: ["Theo", "Peter Jr.", "Ms. K"]
+
+      people.map do |person|
+        #Check if person is added to hash already. If empty, add person to top level
+        if pigeon_list[person].nil?
+          pigeon_list[person] = {}
+        end
+
+        pigeon_list[person][attribute] ||= []
+        pigeon_list[person][attribute].push(details.to_s)
+      end
+    end
+  end
+  pigeon_list
+  # binding.pry
 end

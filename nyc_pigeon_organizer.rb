0859f63455fcebd057b6d68be9cee0bf20e63e18
github username <github email address>
2018-07-13 14:41:50 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attributes, attributes_hash|
+    attributes_hash.each do |type, name|
+      name.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][attributes] == nil
+          pigeon_list[name][attributes] ||= []
+        end
+          pigeon_list[name][attributes] << type.to_s
+        end
+      end
+    end
+  pigeon_list
 end

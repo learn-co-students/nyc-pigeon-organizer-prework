605a9a35c80e5bc4497fab8ac3566e3633f8b35c
Dina Hafez <dinahafez.8@gmail.com>
2018-07-26 15:19:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = {}
+  data.each do |category, data_items|
+    data_items.each do |attributes, names|
+      names.each do |name|
+        if result[name] == nil
+          result[name] = {}
+          result[name][category] = [attributes.to_s]
+        elsif result[name][category] == nil
+          result[name][category] = [attributes.to_s]
+        else
+          result[name][category] << attributes.to_s
+        end
+      end
+    end
+  end
+  result
 end

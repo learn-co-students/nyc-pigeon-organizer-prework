a3345f5f5808961369c61e8f03214e74bea1529e
Justin Wang <justinw827@aol.com>
2018-07-28 04:50:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  newData = Hash.new
+
+  data.each do |key, val|
+    val.each do |key2, val2|
+      val2.each do |pigeon|
+        if newData.include?(pigeon) # Check if pigeon exists in newHash
+          if newData[pigeon].include?(key) # Check if pigeon has the category yet
+            #binding.pry
+            newData[pigeon][key] << key2.to_s
+          else
+            newData[pigeon][key] = [key2.to_s]
+          end
+        else
+          newData[pigeon] =  {key => [key2.to_s]}
+          #binding.pry
+        end
+      end
+    end
+  end
+
+  #binding.pry
+  return newData
 end

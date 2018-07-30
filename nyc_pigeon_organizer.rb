8034c88b761dad43e8c302b13ee39e204628b010
Ciaran Morinan <cmorinan@gmail.com>
2018-07-30 18:33:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  data.each do |attribute, details|
+    details.each do |detail, name_array|
+      name_array.each do |name|
+        pigeons[name] ||= {}
+        pigeons[name][attribute] ||= []
+        pigeons[name][attribute] << detail.to_s
+      end
+    end
+  end
+  pigeons
 end

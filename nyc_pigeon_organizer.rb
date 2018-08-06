442ba0b23a787bcec315d5366250d4b4833b62dd
Trevor Jameson <tjameson15@gmail.com>
2018-08-06 11:05:49 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,27 +20,25 @@ pigeon_data = {
 }
 
 def nyc_pigeon_organizer(data)
-  pigeons = {}
+  pigeon_list = {}
   data.each do |trait_key, trait_value|
     trait_value.each do |atr_key, atr_value|
       atr_value.each do |name|
-        sym_name = name.to_sym
-
         # If the name of the current bird is not a key in pigeons array, add it
-        if !pigeons.keys.include?(name)
-          pigeons[sym_name] = {}
+        if !pigeon_list.keys.include?(name)
+          pigeon_list[name] = {}
         end
         # If current trait is not a key of current bird, add it, with the attribute
-        if !pigeons[sym_name].keys.include?(trait_key)
-          pigeons[sym_name][trait_key] = [atr_key.to_s]
+        if !pigeon_list[name].keys.include?(trait_key)
+          pigeon_list[name][trait_key] = [atr_key.to_s]
         # If current trait is a key of current bird, only add the attribute
-        elsif pigeons[sym_name].keys.include?(trait_key)
-          pigeons[sym_name][trait_key] << atr_key.to_s
+        elsif pigeon_list[name].keys.include?(trait_key)
+          pigeon_list[name][trait_key] << atr_key.to_s
         end
       end
     end
   end
-  binding.pry
+  pigeon_list
 end
 
 nyc_pigeon_organizer(pigeon_data)

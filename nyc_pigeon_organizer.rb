e340c79eba971160c22bb59ad12653d5e9560b92
Jacob Berman <jacobberman1995@gmail.com>
2018-08-17 01:53:37 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,28 +3,21 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   organized = {}
   array = []
-  testr = []
+
   data.each do |trait, value|
     value.each do |color, pidgeons|
       pidgeons.each do |pidgeon|
-      array.push(pidgeon)
+        organized[pidgeon] = {}
       end 
     end 
   end 
-    pidgeons = array.uniq
-    pidgeons.each do |pidgeon|
-      organized[pidgeon] = {}
+   data.each do |trait, value|
+    value.each do |color, pidgeons|
+      pidgeons.each do |pidgeon|
+        organized[pidgeon][trait] = []
+      end 
     end 
-#   organized.each do |pidgeon|
-#   data.each do |trait, value|
-#     value.each do |description, pidgeons|
-#       if pidgeons.include?(pidgeon)
-#         testr.push(organized[pidgeon][trait])
-#         testr.push(description)
-#       end 
-#     end 
-#   end
-# end 
-organized
-    # binding.pry
-end
+  end 
+  
+  binding.pry
+end 

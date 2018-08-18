aee22007e60aa7e6f0fd63f74291e7ead9c2b77f
danmj <daniel.jung@stern.nyu.edu>
2018-08-18 23:43:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeonhash = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !pigeonhash.has_key?(name)
+          pigeonhash[name] = {}
+        end
+
+        if !pigeonhash[name].has_key?(property)
+          pigeonhash[name][property] = []
+        end
+
+        if !pigeonhash[name][property].include?(attribute)
+          pigeonhash[name][property] << attribute.to_s
+        end
+      end
+    end
+  end
+  pigeonhash
 end

5876edb981aba603112bfc2a2f17396f13af21a6
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-19 23:16:40 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,15 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
-# binding.pry 
   list = {}
+#  binding.pry 
   data.each do |data_attribute_symbol, data_attribute_hash|
+# binding.pry
     data_hash.each do |data_descriptor_symbol, data_names_array|
+ binding.pry
       data_names_array.each do |data_name_string|
-# binding.pry        
-        list[data_name_string] = data_attribute_symbol
+ #binding.pry        
+       # list[data_name_string] = data_attribute_symbol
 #binding.pry        
         #list = {data_name_string => {data_attribute_symbol => [data_descriptor_symbol.to_s]}}
       

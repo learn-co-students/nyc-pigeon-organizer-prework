03566e77fdc5aca8ea2e7ad542544d27fbcfc1a9
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-20 05:39:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,16 +6,19 @@ def nyc_pigeon_organizer(data)
     attribute_hash.each do |descriptor_symbol, names_array|
       names_array.each do |name_string|
       
-        descriptor_string = descriptor_symbol.to_s
-        list[name_string] = {}
-      #    list[name_string][attribute__symbol] = []
-          if list[name_string][attribute__symbol] == nil  
-            list[name_string][attribute_symbol] = [descriptor_string]
-          else  list[name_string][attribute_symbol] << descriptor_string 
-          end 
-      # data_name_string => {data_attribute_symbol => [data_descriptor_string]}
+        descriptor_string = descriptor_symbol.to_s 
+        if list[name_string] == nil
+          list[name_string] = {attribute_symbol => [descriptor_string]}
+        else list[name_string][attribute_symbol] =   [descriptor_string] 
+        end 
+        
+        
+      # name_string => {attribute_symbol => [descriptor_string]}
+       
       end
+       
     end
+     
   end 
   list 
   binding.pry 

da4f1b2f2cf0eb8351d6cbb5de539b732db33685
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-20 13:18:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,3 +19,21 @@ def nyc_pigeon_organizer(data)
   end 
   list 
 end
+
+#def nyc_pigeon_organizer(data)
+#  list = {}
+#  data.each do |attribute_symbol, attribute_hash|
+#    attribute_hash.each do |descriptor_symbol, names_array|
+#      names_array.each do |name_string|
+#        descriptor_string = descriptor_symbol.to_s 
+#        if list[name_string] == nil
+#          list[name_string] = {attribute_symbol => [descriptor_string]}
+#        else list[name_string][attribute_symbol] =   [descriptor_string] 
+#        end 
+      # name_string => {attribute_symbol => [descriptor_string]}
+#      end
+#    end
+#  end 
+#  list 
+  # binding.pry Perfect escept only the 2nd (and last) color for those with multiple colors.
+#end

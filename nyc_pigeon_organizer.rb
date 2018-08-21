da869251ba35a6fb974d01ee4c4b7086943df99c
gwatson86 <gwatson86@gmail.com>
2018-08-21 15:11:46 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  pigeon_names = []
+
+  data.each do |attribute, type|
+    type.each do |type, name|
+      pigeon_names << name
+    end
+  end
+
+  pigeon_names = pigeon_names.flatten.uniq
+
+  pigeon_names.each do |bird_name|
+    pigeon_list[bird_name] = {}
+  end
+
+  pigeon_names.each do |bird_name|
+    pigeon_list[bird_name][:color] = []
+    pigeon_list[bird_name][:gender] = []
+    pigeon_list[bird_name][:lives] = []
+  end
+
+  pigeon_list.each do |bird_name|
+    data.each do |attribute, type|
+      type.each do |type, name|
+        if name.include?(bird_name[0])
+          pigeon_list[bird_name[0]][attribute] << type.to_s
+        end
+      end
+    end
+  end
 end

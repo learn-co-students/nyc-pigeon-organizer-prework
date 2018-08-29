5f887554b509fe3e8e4491e39d9c84e8b03a5789
brianyu18 <brianyu1990@gmail.com>
2018-08-29 20:54:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+newhash = {}
+  data.each do |attribute, hash|
+    hash.each do |type, array|
+      array.each do |name|
+        if !newhash.has_key? (name)
+          newhash[name] = {}
+        end
+        if !newhash[name].has_key? (attribute)
+          newhash[name][attribute] = []
+        end
+        if !newhash[name][attribute].include? (type)
+          newhash[name][attribute] << type.to_s
+        end
+      end
+    end
+  end
+return newhash
 end

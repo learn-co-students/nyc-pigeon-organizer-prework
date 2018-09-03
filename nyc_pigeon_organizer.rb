e0102246365f687510d20f26273d0b0f89030043
Ed Putans <edputans@gmail.com>
2018-09-03 17:52:22 +0300

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,49 @@
+# write your code here!pigeon_data = {
+#  :color => {
+#    :purple => ["Theo", "Peter Jr.", "Lucky"],
+#    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+#    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+#    :brown => ["Queenie", "Alex"]
+#  },
+#  :gender => {
+#    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#    :female => ["Queenie", "Ms. K"]
+#  },
+#  :lives => {
+#    "Subway" => ["Theo", "Queenie"],
+#    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+#    "Library" => ["Peter Jr."],
+#    "City Hall" => ["Andrew"]
+#  }
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  a={}
+  data.each do |asl,statlist|
+    statlist.each do |stat,namelist|
+      namelist.each do |name|
+      a[name]={}
+    end
+  end
+end
+#binding.pry
+data.each do |asl,statlist|
+  statlist.each do |stat,namelist|
+    namelist.each do |name|
+    a[name][asl]=[]
+    end
+  end
+end
+
+data.each do |asl,statlist|
+  statlist.each do |stat,namelist|
+    namelist.each do |name|
+    a[name][asl] << stat.to_s
+  end
+end
+end
+
+
+puts a
+  return a
 end

3e35e0d7e9f2bc7737cdf90bf4ea5702f2f7c923
srscalia <shelby.scalia@gmail.com>
2018-09-06 14:14:57 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |key1, hash|
+    hash.each do |attribute, array|
+      array.each do |value|
+        if new_hash[value].nil?
+          new_hash[value]={}
+          #binding.pry
+        end
+        new_hash[value][key1]||=[]
+        new_hash[value][key1]<<attribute.to_s
+        #binding.pry
+      end
+    end
+  end
+  new_hash
 end

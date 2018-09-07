fabf27dd2e534b37026fcf11ecfe22766c55fafa
zhifeng ou <zhifengou@hotmail.com>
2018-09-07 15:29:31 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash={}
+  data.each do |key1,value1|
+    value1.each do |key2,value2|
+      value2.each do |name|
+      if !hash.has_key?(name)
+        hash[name]={}
+      end
+      if !hash[name].has_key?(key1)
+      hash[name][key1]=[]
+      end
+      hash[name][key1] << key2.to_s
+    end
+  end
+end
+hash
 end

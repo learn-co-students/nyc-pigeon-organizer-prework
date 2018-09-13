bfaee6a2263e6a3936808197e2d41ad8db12cc9d
github username <github email address>
2018-09-13 17:15:09 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, att_data|
+    if attribute == :color
+      att_data.each do |color, pigeons|
+        pigeons.each do |name|
+          if pigeon_list.keys.include?(name)
+            pigeon_list[name][:color] << color.to_s
+          else
+            pigeon_list[name] ||= {color: []}
+            pigeon_list[name][:color] << color.to_s
+          end
+        end
+      end
+    end
+    if attribute == :gender
+      att_data.each do |gender, gender_types|
+        gender_types.each do |name|
+          pigeon_list[name] = {gender: []}
+          pigeon_list[name][:gender] << gender.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end

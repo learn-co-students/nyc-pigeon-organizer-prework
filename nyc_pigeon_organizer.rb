4c797487bf0545ea409c78d8da773f4497d235d5
Asaf Davidov <adavidov@conncoll.edu>
2018-09-13 15:55:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,14 +23,18 @@ def nyc_pigeon_organizer(data)
   data.each do |bird_category, bird_data|
     bird_data.each do |bird_info, birds|
       birds.each do |bird|
+        if returned_bird_hash[bird] == nil
+          returned_bird_hash[bird] =
+              {bird_category => bird_data.select{|k,v| v.include?(bird)}.keys.collect{|elem| elem.to_s}}
+        #binding.pry
+        end
         if returned_bird_hash[bird][bird_category] == nil
-        returned_bird_hash[bird] =
-            {bird_category => bird_data.select{|k,v| v.include?(bird)}.keys.collect{|elem| elem.to_s}}
-        binding.pry
+          returned_bird_hash[bird][bird_category] = bird_data.select{|k,v| v.include?(bird)}.keys.collect{|elem| elem.to_s}
+        end
+        #binding.pry
       end
     end
   end
   returned_bird_hash
-  
 end
 nyc_pigeon_organizer(data)

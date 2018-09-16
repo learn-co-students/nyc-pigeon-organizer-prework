c883152b0a4862b347c38cc7e6a32ddc6347897b
suttonau <sutton_mjbc@icloud.com>
2018-09-16 08:35:02 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = { }
+
+ data.each do |category, hash|
+   hash.each do |property, array|
+     array.each do |name|
+        new_hash[name] ||= { }
+        new_hash[name][category] ||= []
+        new_hash[name][category] << property.to_s
+      end
+      end
+    end
+ new_hash
 end

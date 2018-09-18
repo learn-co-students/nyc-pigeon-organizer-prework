eb3a544d4bcd725bc530700156c3fa9b2196d1f4
Rob Parker <into.the.parker@gmail.com>
2018-09-18 11:51:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+
+    
+    new_hash = {}
+
+      data.each do |keys, values|
+        values.each do |k2, v2|
+          v2.each do |i|
+         
+          
+         new_hash[i] ||= {}
+         puts new_hash
+         new_hash[i][keys] ||= []
+         puts new_hash
+         new_hash[i][keys] << k2.to_s
+         puts new_hash
+         puts ''
+          end
+        end 
+      end   
+      new_hash
+    end  

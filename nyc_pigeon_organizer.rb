174d5e56ae1c00d0d625e8446a5e945be8168843
Dario Spina <dario_spina01@live.it>
2018-09-22 22:35:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,16 +4,19 @@ def nyc_pigeon_organizer(data)
     data.each do |category,value_of_category_arr|
       value_of_category_arr.each do |singlevalue,array_name|
         array_name.each do |pidgeon_name|
-                binding.pry
 
-          if pidgeon_list.has_key?(pidgeon_name)
-            pidgeon_list[pidgeon_name][category] << singlevalue
-        
-      else
-        pidgeon_list[pidgeon_name][category] = singlevalue
+          if !pidgeon_list.has_key?(pidgeon_name)
+            pidgeon_list[pidgeon_name]={}
+          end
+            if !pidgeon_list[pidgeon_name].has_key?(category)
+              pidgeon_list[pidgeon_name][category]=[]
+            end
+            if !pidgeon_list[pidgeon_name][category].include?(singlevalue)
+              pidgeon_list[pidgeon_name][category]<< singlevalue.to_s
+        end
     end
     end
     end
-    end
+    
     pidgeon_list
 end

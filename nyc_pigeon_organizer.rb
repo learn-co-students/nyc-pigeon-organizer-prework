68ea6135f610f913fad875771afacb3e62b299fd
github username <github email address>
2018-09-29 16:58:00 -0400

    NYC Pigeons

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |features, data|
+    data.each do |attribute, names|
+      names.each do |name|
+          pigeon_list[name] ||= {} #||= is checking whether left h-s is truthy (exists), if not it adds hash, if yes ||= ensures it is not overwritten
+          pigeon_list[name][features] ||= [] # same again but adding an array
+          pigeon_list[name][features] << attribute.to_s
+      end
+    end
+  end
+  pigeon_list
 end

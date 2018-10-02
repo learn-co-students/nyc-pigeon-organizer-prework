0509080bff5616d2f632853a45696bdbb050fe48
Natasha Buck <natashabuck1@gmail.com>
2018-10-02 07:21:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+  data = pigeon_data
+  pigeon_org = {}
+  data.each { |category, identifier|
+    data[category].each { |id, name_array|
+      data[category][id].each { |name|
+        pigeon_org[name] = {}
+      }
+    }
+  }
+  pigeon_org.each { |pigeon, attributes|
+    data.each { |category, identifier|
+      data[category].each { |id, name_array|
+        data[category][id].each { |name|
+          if name == pigeon
+            pigeon_org[pigeon][category] = []
+          end
+        }
+      }
+    }
+  }
+  pigeon_org.each { |pigeon, attributes|
+    data.each { |category, identifier|
+      data[category].each { |id, name_array|
+        if name_array.include?(pigeon)
+          pigeon_org[pigeon][category] << id.to_s
+        end
+      }
+    }
+  }
+  pigeon_org
 end

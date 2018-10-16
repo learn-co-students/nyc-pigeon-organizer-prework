f30f21f68d917d8ab144890b2b5a8ed470aa03b0
leannemcabey <leanne.m.cabey@gmail.com>
2018-10-16 14:49:50 -0400

    completed #nyc_pigeon_organizer method to pass all tests

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_data = {}
+  data.each do |attribute, value|
+    value.each do |value, birds|
+      birds.each do |bird|
+        if organized_data.keys.include?(bird) == false
+          organized_data[bird] = {attribute => [value.to_s]}
+        else
+          if organized_data[bird].keys.include?(attribute)
+            organized_data[bird][attribute] << value.to_s
+          else
+            organized_data[bird][attribute] = [value.to_s]
+          end
+        end
+      end
+    end
+  end
+  organized_data
 end

f0a9a7bb722a61544bb7cde805ce75a37c09bedb
github username <github email address>
2018-10-18 11:23:12 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,43 +4,11 @@ def nyc_pigeon_organizer(data)
   data.each do |label, details|
     details.each do |key, value|
       value.each do |bird|
-        pigeon_hash[bird] = {:color => [], :gender => [], :lives => []}
+        pigeon_hash[bird] ||= {}
+        pigeon_hash[bird][label] ||= []
+        pigeon_hash[bird][label] << key.to_s
       end
     end
   end
-
-  data[:color].each do |color, bird|
-    bird.each do |bird|
-      pigeon_hash.keys.each do |name|
-
-        if bird == name
-          pigeon_hash[name][:color] << color.to_s
-        end
-      end
-    end
-  end
-
-  data[:gender].each do |gender, bird|
-    bird.each do |bird|
-      pigeon_hash.keys.each do |name|
-
-        if bird == name
-          pigeon_hash[name][:gender] << gender.to_s
-        end
-      end
-    end
-  end
-
-  data[:lives].each do |home, bird|
-    bird.each do |bird|
-      pigeon_hash.keys.each do |name|
-
-        if bird == name
-          pigeon_hash[name][:lives] << home.to_s
-        end
-      end
-    end
-  end
-  
   pigeon_hash
 end

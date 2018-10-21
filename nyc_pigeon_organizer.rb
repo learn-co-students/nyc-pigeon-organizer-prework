252c386cf11f3961ae32b8e582442ca925676627
Robert Schulte <schulte.robertw@outlook.com>
2018-10-21 17:24:44 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do | attribute, attribute_data |
+    attribute_data.each do | descriptor, name_array |
+      name_array.each do | name |
+        if !new_hash[name]
+          new_hash[name] = { attribute => [descriptor.to_s] }
+        elsif !new_hash[name][attribute]
+          new_hash[name][attribute] = [descriptor.to_s]
+        else
+          new_hash[name][attribute] << descriptor.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end

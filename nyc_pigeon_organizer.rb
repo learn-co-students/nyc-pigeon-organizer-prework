b2a1fd416da270954f0ae107b4953d24c66847e4
Manon Pilaud <mandyypilaud@gmail.com>
2018-10-22 20:03:16 -0700

    C1

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,36 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_hash = {}
+  data.each do |category,hash|
+    hash.each do |key,array|
+      array.each do |name|
+        organized_hash[name]= {:color => [], :gender => [], :lives => []}
+
+      end
+    end
+  end
+  data[:color].each do |key,array|
+    array.each do |name|
+      if data[:color][key].include?(name)
+        organized_hash[name][:color]<<key.to_s
+      end
+    end
+  end
+
+  data[:gender].each do |key,array|
+    array.each do |name|
+      if data[:gender][key].include?(name)
+        organized_hash[name][:gender]<<key.to_s
+      end
+    end
+  end
+
+  data[:lives].each do |key,array|
+    array.each do |name|
+      if data[:lives][key].include?(name)
+        organized_hash[name][:lives]<<key.to_s
+      end
+    end
+  end
+  organized_hash
 end

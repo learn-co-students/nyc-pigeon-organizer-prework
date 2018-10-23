cd427ea01984b0096a4b8e75e73336f3e43cd42e
heather sterman <heather.sterman531@gmail.com>
2018-10-23 20:50:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_names = []
+  pigeon_list = {"" => { :a => []}}
+    # "Theo" => {},
+    # "Peter Jr." => {},
+    # "Lucky" => {},
+    # "Ms. K" => {},
+    # "Queenie" => {},
+    # "Andrew" => {},
+    # "Alex" => {},
+  # }
+  data.each do |category, subcategory|
+    if category == :gender
+      subcategory.each do |x, y|
+        y.each do |z|
+          pigeon_list[z] = z
+          # binding.pry
+        end
+      end
+    end
+  end
+  data.each do |category, subcategory|
+    subcategory.each do |a, b|
+        pigeon_list.each do |lcat, lsub|
+          # binding.pry
+          pigeon_list[lcat] = category
+          b.each do |c|
+            if c == lcat
+              # pigeon_list[lcat] << c
+              # binding.pry
+            end
+          end
+          # binding.pry
+        end
+    end
+  end
+  binding.pry
+  pigeon_list
+end
 end

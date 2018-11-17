5d9b235865ddad271b68d1093c0e31d937f2cc51
Kiley Bobbitt <jkileybob@aol.com>
2018-11-17 19:22:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,9 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  pigeon_list = {}
+  data.each do | key1, value1 |
+    value1.each do | key2, value2array |
+      value2array.each do | name |
+        pigeon_list[name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
+  end

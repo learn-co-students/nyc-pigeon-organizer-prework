3eaa74812a7516f81e146dd4c103fd472c58f05b
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 08:28:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,31 +2,44 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
+  
+#first section creates hash with keys of birds names
   data.each do |cgl, pdata|
- #   binding.pry
+#   binding.pry
     pdata.each do |identifier, name|
       for i in 0...name.length
- #     binding.pry
-        if new_hash[name[i]] == nil
-          new_hash[name[i]] = [cgl]
-        else
-          new_hash[name[i]] += [cgl]
+#     binding.pry
+        new_hash[name[i]] = {}
+#        binding.pry
+      end
+    end
+  end
+#  binding.pry
+
+# new_hash = {
+# "Theo"=>{},
+# "Peter Jr."=>{},
+# "Lucky"=>{},
+# "Ms. K"=>{},
+# "Queenie"=>{},
+# "Andrew"=>{},
+# "Alex"=>{}
+# }
+
+#second piece enumerates over data and puts in new name hash
+  data.each do |cgl, pdata|
+    new_hash.keys.each do |names|
+#      binding.pry
+      new_hash[names][cgl] = []
+      pdata.each do |identifier, name|
+#        binding.pry
+        for i in 0...name.length
+          if name[i] == names
+            new_hash[names][cgl].push(identifier.to_s)
+          end
         end
       end
     end
   end
-  new_hash.each do |name, cgl|
-    new_hash[name] = cgl.uniq
-    binding.pry
-  end
+  new_hash
 end
-
-
-
-{"Theo"=>[:color, :gender, :lives],
- "Peter Jr."=>[:color, :gender, :lives],
- "Lucky"=>[:color, :gender, :lives],
- "Ms. K"=>[:color, :gender, :lives],
- "Queenie"=>[:color, :gender, :lives],
- "Andrew"=>[:color, :gender, :lives],
- "Alex"=>[:color, :gender, :lives]}

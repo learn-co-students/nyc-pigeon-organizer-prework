ad5a88a0d3369b5c031523e71564bec5fc02d21b
MichaelM3 <dagreekarican@aim.com>
2018-11-18 11:12:11 -0500

    finished, i need to take note of this lesson, as it was good practice

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |info, names|
+    names.each do |attribute, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+        if !new_hash[name].has_key?(info)
+          new_hash[name][info] = []
+        end
+        if !new_hash[name][info].include?(attribute)
+          new_hash[name][info] << attribue.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end

adea353bb8c01cca1409d77e5ffbdf2db8439659
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-20 19:49:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,8 +5,9 @@ def nyc_pigeon_organizer(data)
     info.each do |sub_info, names|
       names.each do |name|
         pigeon_list[name] ||= {}
-        pigeon_list[name][attribute] = sub_info.to_s
-        binding.pry
+        pigeon_list[name][attribute] ||= []
+        pigeon_list[name][attribute] << sub_info.to_s
+        #binding.pry
       end
     end 
   end

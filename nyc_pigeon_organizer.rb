2284c9867f592d01dd4579398819cdfd46ffa6ec
Dania Herrera <danherrera0@gmail.com>
2018-12-01 04:03:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_data.each do |color,gender,lives|
+    gender.each do |male, female|
+    names= male.values
+    female_names=female.values
+names.push(female_names.flatten!)
+      pigeon_list={}
+      names.each do |pigeon_name|
+        pigeon_list << pigeon_name
+      pigeon_list[pigeon_name][color]=
+      pigeon_data.each do |color,gender,lives|
+      color
+      pigeon_list[pigeon_name][gender]=
+      pigeon_data.each do |color,gender,lives|
+      gender
+      pigeon_list[pigeon_name][lives]=
+      pigeon_data.each do |color,gender,lives|
+      lives
 end

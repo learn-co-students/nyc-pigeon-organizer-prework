feac204c25cc671206e94c48d2924c1282c23ac2
Dania Herrera <danherrera0@gmail.com>
2018-12-02 23:57:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,30 @@
+require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
-  pigeon_data.each do |color,gender,lives|
-    gender.each do |male, female|
-    names= male.values
-    female_names=female.values
-names.push(female_names.flatten!)
-      pigeon_list={}
-      names.each do |pigeon_name|
-        pigeon_list << pigeon_name
-      pigeon_list[pigeon_name][color]=
-      pigeon_data.each do |color,gender,lives|
-      color
-      pigeon_list[pigeon_name][gender]=
-      pigeon_data.each do |color,gender,lives|
-      gender
-      pigeon_list[pigeon_name][lives]=
-      pigeon_data.each do |color,gender,lives|
-      lives
+pigeon_list ={}
+  pigeons=[]
+    data.each do |attributes,type_hash|
+      type_hash.each do |type_symbol,values_array|
+      if attributes == :gender
+        {|values_array| pigeons << values_array }
+        binding.pry
+#     gender.each do |male, female|
+#     names= male.values
+#     female_names=female.values
+# names.push(female_names.flatten!)
+#       pigeon_list={}
+#       names.each do |pigeon_name|
+#         pigeon_list << pigeon_name
+#       pigeon_list[pigeon_name][color]=
+#       data.each do |color,gender,lives|
+#       color
+#       pigeon_list[pigeon_name][gender]=
+#       data.each do |color,gender,lives|
+#       gender
+#       pigeon_list[pigeon_name][lives]=
+#       data.each do |color,gender,lives|
+#       lives
+end
+end
+end
 end

d9b14ab780d92c48b7645d8d904a1c93577ca76a
Dania Herrera <danherrera0@gmail.com>
2018-12-03 01:45:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,30 +1,30 @@
 require "pry"
+# def nyc_pigeon_organizer(data)
+# pigeon_list ={}
+#   pigeons=[]
+#     data.each do |attributes,type_hash|
+#       type_hash.each do |type_symbol,values_array|
+#           binding.pry
+#       if attributes == :gender
+#         pigeons << values_array
+#         pigeons = pigeons.flatten
+#         pigeons.each do |name|
+#           pigeon_list[name]={attribues}
+#
+# end
+# end
+# end
+# end
+# end
 def nyc_pigeon_organizer(data)
-  # write your code here!
-pigeon_list ={}
-  pigeons=[]
-    data.each do |attributes,type_hash|
-      type_hash.each do |type_symbol,values_array|
-      if attributes == :gender
-        {|values_array| pigeons << values_array }
-        binding.pry
-#     gender.each do |male, female|
-#     names= male.values
-#     female_names=female.values
-# names.push(female_names.flatten!)
-#       pigeon_list={}
-#       names.each do |pigeon_name|
-#         pigeon_list << pigeon_name
-#       pigeon_list[pigeon_name][color]=
-#       data.each do |color,gender,lives|
-#       color
-#       pigeon_list[pigeon_name][gender]=
-#       data.each do |color,gender,lives|
-#       gender
-#       pigeon_list[pigeon_name][lives]=
-#       data.each do |color,gender,lives|
-#       lives
-end
-end
-end
+  data.each_with_object({}) do |(key, hash), result|
+    hash.each do |value, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << value.to_s
+          # binding.pry
+      end
+    end
+  end
 end

ee42baf7a49307f7d0c8e6d26634e3cf9f7db67c
MatthewBrophy <Brophy3030@gmail.com>
2018-12-13 11:37:32 -0800

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, info|
+    info.each do |sub_info, names|
+        names.each do |name|
+          pigeon_list[name] ||= {}
+          pigeon_list[name][attribute] ||= []
+          pigeon_list[name][attribute] << sub_info.to_s
+        # binding.pry
+        # pigeon_key = named_pigeon
+        
+        # pigeon_list[pigeon_key] = "test"
+        
+      end
+    end
+  end
+  pigeon_list
 end
+
+# If it is something to be shoveled, it needs to be saved as something.

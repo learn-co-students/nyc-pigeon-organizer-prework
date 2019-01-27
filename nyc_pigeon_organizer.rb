6c6c4af2e2a16d79fc4e5efabaf1cca3a0c57ae6
Eric Hardiman <hardimaneric@gmail.com>
2019-01-27 01:49:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organize = {}
+
+  data.each do |attribute, subdata|
+    subdata.each do |otherdata, pigeons|
+      pigeons.each do |pigeon|
+        organize[pigeon] ||= {}
+        organize[pigeon][attribute] ||= []
+        organize[pigeon][attribute] << otherdata.to_s
+      end
+    end
+  end
+  organize
 end

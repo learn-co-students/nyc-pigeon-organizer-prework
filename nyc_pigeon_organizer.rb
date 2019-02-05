0bf38bcdffe5fa47bd4d5fb3b4a9d8b35fbcdb91
es6ix <46784380+es6ix@users.noreply.github.com>
2019-02-05 03:53:27 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized = {}
+
+  data.each { |attribs, hashes|
+    hashes.each { |characteristic, names|
+      names.each { |name|
+        organized[name] = organized[name] ? organized[name] : {}
+        organized[name][attribs] = organized[name][attribs] ? organized[name][attribs] : []
+        organized[name][attribs] << characteristic.to_s
+      }
+    }
+  }
+  organized
 end

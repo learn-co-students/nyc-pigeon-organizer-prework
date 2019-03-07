00e54d5e24b08a0e98671c26f06a4d2ea5a08656
ryanden2018 <denlinger.ryan@gmail.com>
2019-03-06 18:11:48 -0600

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  results = {}
+  data.each do |property_name,property_data|
+    property_data.each do |property_value,names|
+      names.each do |name|
+        if !results[name]
+          results[name] = {}
+        end
+        if !results[name][property_name]
+          results[name][property_name] = []
+        end
+        if !results[name][property_name].include?(property_value)
+          results[name][property_name] << property_value.to_s
+        end
+      end
+    end
+  end
+  results
 end

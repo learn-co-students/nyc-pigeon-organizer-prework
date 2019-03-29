6c571eb0de8890ab976ccd4e61e0647195d0b9cd
David zhang <d.ydzdavid@yahoo.com>
2019-03-29 19:01:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # write your code here
+  new_data = {}
+  data.each do |info, key|
+    key.each do |key_one, value|
+      value.each do |names|
+        if not new_data.has_key?(names)
+          new_data[names] = {}
+        end
+        
+        if not new_data[names].has_key?(info)
+          new_data[names][info] = []
+        end
+        
+        new_data[names][info] << key_one.to_s
+      end
+    end
+  end
+  new_data
 end

11c5359da714c53392fcd2ec044491d9e87c8337
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-05 17:14:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |key, value|
+    value.each do |color, names|
+      names.each do |name|
+        if pigeon_list[name].nil?
+          pigeon_list[name] = key
+        end
+        #binding.pry
+      end
+    end
+  end
+  pigeon_list
 end

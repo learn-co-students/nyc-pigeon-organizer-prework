df4477270b60db1b74045ee70e92956e60d64a2d
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-05 18:01:44 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,13 +4,19 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
   data.each do |key, value|
-    value.each do |color, names|
+    value.each do |pigeon_trait, names|
       names.each do |name|
         if pigeon_list[name].nil?
-          pigeon_list[name] = key
+            pigeon_list[name] = {}
         end
-        #binding.pry
-      end
+
+        if pigeon_list[name][key].nil?
+              pigeon_list[name][key] = []
+              pigeon_list[name][key] << pigeon_trait.to_s
+        else
+              pigeon_list[name][key] << pigeon_trait.to_s
+          end
+        end
     end
   end
   pigeon_list

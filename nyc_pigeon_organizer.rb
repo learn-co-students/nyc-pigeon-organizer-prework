d28cc399bc92fba5160a5e452b1761738a01fa57
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-25 21:54:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |thing, details|
+    details.each do |subthing, names|
+      names.each do |name|
+        new_hash[name] ||= {}
+        new_hash[name][thing] ||= []
+        new_hash[name][thing] << subthing.to_s
+      end
+    end
+  end
+  new_hash
+
 end

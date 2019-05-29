6e1db8d8b1566d1d20b4e4594651604f3b901ca7
Conrad Casper <conradcasper@icloud.com>
2019-05-29 01:42:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+  pigeon_names = []
+  
+  data.each do |category, value|
+    value.each do |key, names|
+      pigeon_names.push(names)
+    end 
+  end 
+  pigeon_names = pigeon_names.flatten.uniq
+  
+  pigeon_names.each do |name| 
+    pigeon_hash[name] = {}
+  
+  data.each do |category, values|
+    pigeon_hash[name][category] = []
+    
+    values.each do |key, names|
+      if names.include?(name)
+        pigeon_hash[name][category].push(key.to_s)
+      end 
+    end 
+  end 
+end 
+  pigeon_hash
 end

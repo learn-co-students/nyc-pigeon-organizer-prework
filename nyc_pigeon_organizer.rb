0c0dc0a5bd7d7acab759a04c28f2967a3b7cd351
brinda behal <brindab50@gmail.com>
2019-09-07 19:46:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |prpperty, hash|
+    hash.each do |attirbute, array|
+      array.each do |name|
+        
 end
+end 
+end 
+end 

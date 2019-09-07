e0c64bbbf8290fdc29c0dcff152208286a6ad757
brinda behal <brindab50@gmail.com>
2019-09-07 22:52:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,20 @@
 require'pry'
 def nyc_pigeon_organizer(data)
-
-  data.each_with_object({}) do |(key, hash),pigeon_list|
-     
-      hash.each do |k , v|
-        v.each do |name|
-          pigeon_list[name] = {}
-           binding.pry 
-          
-         pigeon_list[name] << v
+ data.each_with_object({}) do |(key, hash),pigeon_list|
+    hash.each do |k , v|
+      v.each do |name|
+      if pigeon_list[name] == nil 
+        pigeon_list[name] = {}
+      end 
+       if pigeon_list[name][key] == nil 
+        pigeon_list[name][key] = []
+      end 
+          # binding.pry 
+         pigeon_list[name][key] << k.to_s
+      
         
-        
-       
-     
-     
-     
-        
-     end
+      end
     end 
   end
 end 
+
